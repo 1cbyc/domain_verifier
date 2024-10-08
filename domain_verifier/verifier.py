2f59863c653063bc0c5894fa25d8145f4682b22e
1cbyc <isaacnsisong@gmail.com>
2024-10-08 01:07:16 +0100

    building a cli domain verifier with python

---
+++ domain_verifier/verifier.py
@@ -0,0 +1,20 @@
+# domain_verifier/verifier.py
+
+import requests
+import validators
+
+class DomainVerifier:
+    def __init__(self, domain):
+        self.domain = domain
+
+    def is_valid(self):
+        """Check if the domain is valid."""
+        return validators.domain(self.domain)
+
+    def is_reachable(self):
+        """Check if the domain is reachable."""
+        try:
+            response = requests.get(f'http://{self.domain}', timeout=5)
+            return response.status_code == 200
+        except requests.RequestException:
+            return False
